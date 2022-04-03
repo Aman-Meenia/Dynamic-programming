1184c5bc87f6df63c39b1ce5573e4746e054410d
Aman-Meenia <79632247+Aman-Meenia@users.noreply.github.com>
2022-04-03 18:31:33 +0530

    Update problem8.cpp

--- problem8.cpp
+++ problem8.cpp
@@ -12,26 +12,35 @@ void fun(vector<int> &v, int i){
 
 // memoized code
 
-int fun(vector<int> &v, int i,vector<int> &dp){
-    if(i==0){
-        return v[i];
-    }else if(i<0){
-        return 0;
+class Solution {
+public:
+int dp[101];
+    int fun(vector<int> &v, int i){
+        if(i==0){
+            return v[i];
+        }
+        if(i<0){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+//         we have two option if we rob the nth house we cannot ron the n-1 because we cannot rob two continous house that's why we move to n-2 house
+//         if we not rob nth house then we rob n-1 house
+        return dp[i]=max(v[i]+fun(v,i-2),fun(v,i-1));
     }
-    if(dp[i]!=-1){
-        return dp[i];
+    int rob(vector<int>& v) {
+      
+        memset(dp,-1,sizeof dp);
+        int size=v.size();
+          
+        return fun(v,size-1);
     }
-    return dp[i]=max(v[i]+fun(v,i-2,dp),fun(v,i-1,dp));
-}
-int maximumNonAdjacentSum(vector<int> &v){
-    int size=v.size();
-    vector<int> dp(size,-1);
- return fun(v,size-1,dp);
-}
+};
 // tabulation
 
 
-int maximumNonAdjacentSum(vector<int> &v){
+  int rob(vector<int>& v){
     int size=v.size();
     vector<int> dp(size,-1);
     dp[0]=v[0];
